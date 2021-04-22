7742a4b9aedcdd5eb2809c76e9038a454df996c0
MaratBR <marat01q@gmail.com>
2021-04-22 18:10:57 +0700

    remove SW

--- Shorty/ClientApp/src/index.js
+++ Shorty/ClientApp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -13,5 +13,5 @@ ReactDOM.render(
   </HashRouter>,
   rootElement);
 
-registerServiceWorker();
+unregister();
 
